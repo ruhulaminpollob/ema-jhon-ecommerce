9c48dd0814bb53deffaaa0953531c595e1df72ff
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-28 15:48:54 +0600

    added header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div className=''>
-            <h1>hello from header</h1>
-            
-        </div>
+        <nav className='h-20 bg-primary flex justify-between items-center md:px-20'>
+            <img src={logo} alt="" />
+            <div className='nav-link flex gap-5  text-white '>
+                <a href="/order">Order</a>
+                <a href="/orderReview">Order Review</a>
+                <a href="/manageInventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+
+
+        </nav>
     );
 };
 
